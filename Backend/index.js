62b73761a9998e3e7170324a28c54453c14198f8
mohipal92 <mohipalmaahi4050@gmail.com>
2026-03-31 20:26:42 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const port=process.env.PORT || 8000
+const app = express()
+ 
+
+app.get('/', (req, res) => {
+  res.send('Hello World! DevConnect app is running. ')
+})
+
+app.listen(port, () => {
+  console.log(`DevConnect app  is listening on port ${port}`)
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        There is no going back now, the frontend is here to stay!
+      </h1>
+    </div>
+  )
+}
+
+export default App
