73aa467ec2e179c6803f1fc972318ba3d0fa50cf
mohipal92 <mohipalmaahi4050@gmail.com>
2026-04-01 23:54:08 +0530

    dabase connectedf

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDb from './config/db.js'
 dotenv.config()
 const port=process.env.PORT || 8000
 const app = express()
  
-
+await connectDb();
 app.get('/', (req, res) => {
   res.send('Hello World! DevConnect app is running. ')
 })
